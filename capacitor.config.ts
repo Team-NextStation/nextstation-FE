24f7ad67464554c88a6861eabbafed7174fc01e0
qkrekdls <gunmannduu@gmail.com>
2026-09-06 01:01:17 +0900

    feat: configure Capacitor Android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'nextstation.app',
   appName: '환승여행',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
