47d7802a6f5b185f602dbc80b955a1ed40405793
Eunjin-n <ikee1652@duksung.ac.kr>
2026-09-24 19:20:06 +0900

    feat: 토스트 위치 prop 추가

--- src/pages/course/components/ShowToast.ts
+++ src/pages/course/components/ShowToast.ts
@@ -1,12 +1,16 @@
-import { toast } from "react-toastify";
+import { toast, type ToastPosition } from "react-toastify";
 
 interface ToastProps {
   message: string;
+  position?: ToastPosition;
 }
 
-export const showToast = ({ message }: ToastProps) => {
+export const showToast = ({
+  message,
+  position = "top-center",
+}: ToastProps) => {
   toast(message, {
-    position: "top-center",
+    position,
     autoClose: 1200,
     hideProgressBar: true,
     closeOnClick: true,
